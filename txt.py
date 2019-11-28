8a7f1566e19ab90d1ab5bd41abcff36062f87dec
yashuu24 <52904592+yashuu24@users.noreply.github.com>
2019-11-28 09:22:09 +0530

    Update txt.py

--- txt.py
+++ txt.py
@@ -1,5 +1,20 @@
 # This program adds two numbers
+from flask import Flask
+from redis import Redis, RedisError
+import os
+import socket
 
+redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    try:
+        visits = redis.incr("counter")
+    except RedisError:
+        visits = "<i>cannot connect to Redis, counter disabled</i>"
+        
 num1 = 1.5
 num2 = 6.3
 
@@ -10,4 +25,7 @@ sum = float(num1) + float(num2)
 # Display the sum
 
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
 
